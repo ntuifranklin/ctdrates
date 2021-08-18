5f502354381f6758cfcc4a34768a89fc98729f44
Franklin Nkokam Ngongang <ntuifrankln2005@gmail.com>
2021-08-18 01:13:42 -0400

    trying to fixing the tab issue

--- app/javascript/packs/tabulation.js
+++ app/javascript/packs/tabulation.js
@@ -1,4 +1,9 @@
 
-$('#myTab a[href="#profile"]').tab('show') // Select tab by name
-$('#myTab a[href="#contact"]').tab('show') // Select tab by name
-$('#myTab a[href="#home"]').tab('show') // Select tab by name
+$('#nav-tab a[href="#nav-profile"]').tab('show') // Select tab by name
+$('#nav-tab a[href="#nav-contact"]').tab('show') // Select tab by name
+$('#nav-tab a[href="#nav-home"]').tab('show') // Select tab by name
+
+$('#nav-tab a').on('click', function (e) {
+  e.preventDefault()
+  $(this).tab('show')
+})
