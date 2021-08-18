309a181bfa8a215aa6672ae8bdd0237a322f4a94
Franklin Nkokam Ngongang <ntuifrankln2005@gmail.com>
2021-08-18 01:21:59 -0400

    trying to fixing the tab issue

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,7 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-require("jquery.min")
+require("jquery")
 //import "bootstrap"
 //require("packs/js/jquery.min")
 require("popper.min")
