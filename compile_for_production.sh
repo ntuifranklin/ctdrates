07bec0fa08001b85f555f18af964d454b75355a5
Franklin Nkokam Ngongang <ntuifrankln2005@gmail.com>
2021-08-18 01:04:44 -0400

    good morning

--- compile_for_production.sh
+++ compile_for_production.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 # This scripts starts the rails app  production server
 chmod -R 777 app/javascript/packs
-sleep 1
+sleep 2
 echo "precompiling everything for production environment"
 rake assets:precompile RAILS_ENV=production
 sleep 1
 rake db:migrate RAILS_ENV=production
 sleep 1
 echo "changing file permission for tmp and public folder"
-chmod -R 755 *  ; sleep 1
+chmod -R 755 *  ; sleep 2 ;
 chmod -R 777 tmp  
 chmod -R 777 public
 echo "chaninging ownership and assigning to user ctdrates for root folder"
